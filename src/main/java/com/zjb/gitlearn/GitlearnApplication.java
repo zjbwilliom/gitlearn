1c3915d5cdabe2958f20f58ba51c902c88e89672
zengjiongbiao.co <648878900@qq.com>
2019-08-02 10:14:44 +0800

    commit

---
+++ src/main/java/com/zjb/gitlearn/GitlearnApplication.java
@@ -0,0 +1,13 @@
+package com.zjb.gitlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlearnApplication.class, args);
+    }
+
+}
